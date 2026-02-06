5d48f6460e2c750004fa85fd618f88a004597467
Rajat <rajat804@gmail.com>
2026-02-06 17:29:17 +0530

    server js page

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js"
 import { promises as dns } from "dns";
 import authRoutes from "./routes/authRoutes.js"
-
+import adminRoutes from "./routes/adminRoutes.js"
+import propertyRoutes from "./routes/propertyRoutes.js"
 // Optional (not required now, but ok)
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
@@ -17,9 +18,13 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 // 🔥 AUTH ROUTES (MOST IMPORTANT)
 app.use("/api/auth", authRoutes);
+app.use("/api/admin",adminRoutes);
+app.use("/api/properties",propertyRoutes);
+app.use("/api", propertyRoutes);
 
 
 // Test API
