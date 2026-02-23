38d2c0b2c52465cd15c711abb7fcceef5cf158cb
Rajat <rajat804@gmail.com>
2026-02-23 11:35:06 +0530

    server update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 
 // Routes
@@ -11,7 +11,7 @@ import { promises as dns } from "dns";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
