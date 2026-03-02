69552d96ea5a1e6257cafc5f866a86388199ecd2
Rajat <rajat804@gmail.com>
2026-03-02 12:43:26 +0530

    server update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+// import dotenv from "dotenv/config";
 import connectDB from "./config/mongodb.js";
 
 // Routes
